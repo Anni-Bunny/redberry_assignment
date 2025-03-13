47ce15b052955a5e482b8fa61472272cbe595c60
Ani Maghradze <animaghradze50@gmail.com>
2025-03-14 01:21:02 +0400

    Created AllTasks page and registered the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import {Route, Routes} from "react-router-dom";
+import {AllTasks} from "./pages/AllTasks.tsx";
+import {Header} from "./components/Header.tsx";
 
 function App() {
     return (
         <>
-        <Routes>
-            <Route path="/"/>
-        </Routes>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<AllTasks/>}/>
+            </Routes>
         </>
     );
 }
---
+++ src/pages/AllTasks.tsx
@@ -0,0 +1,13 @@
+import {Container} from "../components/Container.tsx";
+
+export const AllTasks = () => {
+    return (
+        <Container>
+            <h1 className='text-[34px] font-semibold mt-10'>დავალებების გვერდი</h1>
+
+            <div>
+
+            </div>
+        </Container>
+    );
+};
