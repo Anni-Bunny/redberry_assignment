55ce192d336d9429845010d7d9ad2e876d759081
Ani Maghradze <animaghradze50@gmail.com>
2025-03-15 01:10:41 +0400

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function Container({ children, className }: { children: ReactNode; className?: string }) {
     return (
-        <div className={`container w-[1680px] flex mx-auto items-center ${className}`}>
+        <div className={`w-[1680px] flex mx-auto items-center ${className}`}>
             {children}
         </div>
     );
