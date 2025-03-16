aefc66eadddd8b3885f202434e7eb34cda007e89
Ani Maghradze <animaghradze50@gmail.com>
2025-03-17 02:54:22 +0400

    Create API.ts
    created API class and added functions to get data from API using axios

---
+++ src/classes/API.ts
@@ -0,0 +1,59 @@
+import axios, {AxiosInstance} from 'axios';
+import {config} from "../../config.ts";
+
+class API {
+
+    public url: string
+    public token: string
+    public axios: AxiosInstance
+
+    public constructor(url: string, token: string, timeout = 1000) {
+        this.url = url;
+        this.token = token;
+        this.axios = axios.create({
+            baseURL: url,
+            timeout: timeout,
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Accept': 'application/json'
+            }
+        })
+    }
+
+    getStatuses() {
+        return this.axios.get('/statuses');
+    }
+
+    getPriorities() {
+        return this.axios.get('/priorities');
+    }
+
+    getDepartments() {
+        return this.axios.get('/departments');
+    }
+
+    getEmployees() {
+        return this.axios.get('/employees');
+    }
+
+    createEmployee() {
+        return this.axios.post('/employees',{});
+    }
+
+    public getTasks(id?: number) {
+        return this.axios.get('/tasks' + (id ? `/${id}` : ''));
+    }
+
+    public createTask() {
+        return this.axios.post('/tasks', {});
+    }
+
+    public changeTaskStatus({id, status_id}: { id: number, status_id: number }) {
+        return this.axios.put(`/tasks/${id}`, {
+            'status_id': status_id
+        });
+    }
+
+}
+
+export const api = new API(config.url, config.token)
