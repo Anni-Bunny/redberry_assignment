92b9f37661dde1dc5a166eb369facf6d96067ac3
Ani Maghradze <animaghradze50@gmail.com>
2025-03-17 03:05:12 +0400

    Create StatusColumn.tsx

---
+++ src/components/StatusColumn.tsx
@@ -0,0 +1,25 @@
+import {TaskCard} from "./TaskCard.tsx";
+import {Task} from "../interfaces/Task.ts";
+import {Status} from "../interfaces/Status.ts";
+
+const statusColors = ['bg-[#3A86FF]', 'bg-[#F7BC30]', 'bg-[#FB5607]', 'bg-[#FF006E]']
+
+
+export function StatusColumn({status, tasks}: { status: Status, tasks: Task[] }) {
+
+
+    const bgColor = statusColors[(status.id % statusColors.length)];
+
+    return (
+        <div className='flex justify-center flex-col gap-[30px]'>
+            <div
+                className={`w-[381px] h-[54px] flex justify-center items-center rounded-[10px] py-[15px] ${bgColor} text-white text-xl font-medium`}>
+                {status.name}
+            </div>
+
+            {
+                tasks && tasks.map((task) => task.status.id === status.id && <TaskCard key={'task_' + task.id} task={task}/>)
+            }
+        </div>
+    );
+};
