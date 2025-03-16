c99beff155b81a0d51d2824f6355f4a8fb986120
Ani Maghradze <animaghradze50@gmail.com>
2025-03-17 03:04:17 +0400

    Update Departments.tsx
    moved colors from object to array.
    changed parameter to get department

--- src/components/Departments.tsx
+++ src/components/Departments.tsx
@@ -1,40 +1,22 @@
-const departments = {
-    1:{
-        name: 'ადმინისტრ.',
-        bgColor: 'bg-green-400'
-    },
-    2: {
-        name: 'HRM',
-        bgColor: 'bg-[#FF66A8]'
-    },
-    3: {
-        name: 'ფინანსები',
-        bgColor: 'bg-emerald-400'
-    },
-    4: {
-        name: 'მარკეტინგი',
-        bgColor: 'bg-[#FD9A6A]'
-    },
-    5:{
-        name: 'ლოჯოსტიკა',
-        bgColor: 'bg-[#89B6FF]'
-    },
-    6:{
-        name: 'ინფ. ტექ.',
-        bgColor: 'bg-[#FFD86D]'
-    },
-    7:{
-        name: 'მედია',
-        bgColor: 'bg-fuchsia-400'
-    }
-}
+import {Department} from "../interfaces/department.ts";
 
-export type department = keyof typeof departments
+const depColors = [
+    'bg-fuchsia-400',
+    'bg-green-400',
+    'bg-[#FF66A8]',
+    'bg-emerald-400',
+    'bg-[#FD9A6A]',
+    'bg-[#89B6FF]',
+    'bg-[#FFD86D]'
+]
 
-export function Departments ({department = 1}:{department:department}){
+export function Departments({department}: { department: Department }) {
+
+    const selectedColor = depColors[department.id % depColors.length]
+
     return (
-        <div className={`w-[88px] h-6 rounded-[15px] py-[5px] px-[9px] flex gap-x-2.5 text-white font-normal text-xs items-center justify-center ${departments[department].bgColor}`}>
-            {departments[department].name}
+        <div className={` text-nowrap overflow-hidden text-ellipsis w-[88px] h-6 rounded-[15px] py-[5px] px-[9px] text-white font-normal text-xs items-center justify-center ${selectedColor}`}>
+            {department.name}
         </div>
     );
 };
