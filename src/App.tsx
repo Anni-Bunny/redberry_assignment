b8179767c0e00173bd4a454373bb7d7f99afd9b3
Ani Maghradze <animaghradze50@gmail.com>
2025-03-18 14:02:26 +0400

    created CreateNewTask page and registered the route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import {AllTasks} from "./pages/AllTasks.tsx";
 import {Header} from "./components/Header.tsx";
 import {SingleTask} from "./pages/SingleTask.tsx";
+import {CreateNewTask} from "./pages/CreateNewTask.tsx";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<AllTasks/>}/>
                 <Route path="/tasks/:id" element={<SingleTask/>}/>
+                <Route path="/createNewTask" element={<CreateNewTask/>}/>
             </Routes>
         </>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,36 @@
 import {Container} from "./Container.tsx";
 import {Add} from "../assets/icons/Add.tsx";
 import {Logo} from "../assets/Logo.tsx";
+import {useNavigate} from "react-router-dom";
 
 export const Header = () => {
+    const navigate = useNavigate();
+
+    const handleNewTaskClick = () => {
+        navigate("/createNewTask");
+    };
+
+    const handleLogoClick = () => {
+        navigate("/");
+    };
+
+
     return (
         <Container>
             <header className='flex items-center justify-between h-[100px] w-full'>
-                <div>
+                <div onClick={handleLogoClick} className='cursor-pointer'>
                     <Logo/>
                 </div>
 
                 <div className='flex gap-10'>
-                    <button className='text-nowrap border border-[#8338EC] flex items-center justify-center h-10 rounded-[5px] cursor-pointer py-2.5 px-5 w-[225px] font-normal'>თანამშრომლის შექმნა</button>
-                    <button className='bg-[#8338EC] flex items-center justify-center h-10 rounded-[5px] cursor-pointer py-2.5 px-5 gap-1 w-[268px] font-normal text-white'><Add/>შექმენი ახალი დავალება</button>
+                    <button
+                        className='text-nowrap border border-[#8338EC] flex items-center justify-center h-10 rounded-[5px] cursor-pointer py-2.5 px-5 w-[225px] font-normal'>თანამშრომლის
+                        შექმნა
+                    </button>
+                    <button
+                        className='bg-[#8338EC] flex items-center justify-center h-10 rounded-[5px] cursor-pointer py-2.5 px-5 gap-1 w-[268px] font-normal text-white'
+                        onClick={handleNewTaskClick}><Add/>შექმენი ახალი დავალება
+                    </button>
                 </div>
             </header>
         </Container>
