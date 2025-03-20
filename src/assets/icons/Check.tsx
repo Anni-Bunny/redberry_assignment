0033a0807148926a4d418a32d3bf8b2908804784
Ani Maghradze <animaghradze50@gmail.com>
2025-03-20 20:51:44 +0400

    Updated checkbox

--- src/assets/icons/Check.tsx
+++ src/assets/icons/Check.tsx
@@ -1,7 +1,7 @@
-export const Check = () => {
+export const Check = ({className, color}: { className?: string, color?:string }) => {
     return (
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.3327 4L5.99935 11.3333L2.66602 8" stroke="#6C757D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <svg className={className} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.3327 4L5.99935 11.3333L2.66602 8" stroke={color ?? "#6C757D"} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     );
 };
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,21 +1,31 @@
 import React from "react";
+import {Check} from "../assets/icons/Check.tsx";
 
 interface checkboxProps {
     label?: string,
     name?: string,
+    image?: string,
     value?: string | number,
     checked?: boolean,
     onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export function CheckBox({label, name, value, checked = false, onChange}: checkboxProps) {
+export function CheckBox({label, name, image, value, checked = false, onChange}: checkboxProps) {
 
     return (
-        <label className="flex gap-[15px]">
+        <label className="flex items-center gap-[15px]">
             <input type="checkbox" value={value} checked={checked} name={name} onChange={onChange}
                    className="hidden peer"/>
-            <span className="w-[22px] h-[22px] border-[1.5px]  rounded-[6px] peer-checked:border-[#8338EC]"></span>
-            <span className="text-[#474B57]">{label}</span>
+            <span className="w-[22px] h-[22px] border-[1.5px] rounded-[6px] border-[#8338EC] flex items-center justify-center relative">
+                <span className={`${ !checked ? 'hidden' : ''}`}>
+                    <Check color="#8338EC"/>
+                </span>
+            </span>
+            <span className=" flex gap-2.5 items-center text-[#474B57]">
+                {image && <img src={image} alt="img" className='w-[28px] h-[28px] rounded-full'/>}
+
+                {label}
+            </span>
         </label>
     );
 }
