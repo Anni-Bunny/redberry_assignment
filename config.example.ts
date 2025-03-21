13753cf3a9b3653a19b29f2919124a85d140959f
Ani Maghradze <animaghradze50@gmail.com>
2025-03-21 22:02:38 +0400

    Getting env ready for vercel

--- config.example.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-  "url": "https://momentum.redberryinternship.ge/api",
-  "token": "Generate from: https://momentum.redberryinternship.ge/generate-token"
-}
---
+++ config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  url: import.meta.env.VITE_API_URL || '',
+  token: import.meta.env.VITE_API_TOKEN || ''
+}
